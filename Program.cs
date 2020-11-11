ede3db0855697d59a16fb2bd015a1b6d073302eb
YuriiTrofimenko <tyaa@ukr.net>
2020-11-11 16:32:36 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
